ae9a559c81102a0048ddc9a87e4dbe1b9c246921
paul <pgtech246@gmail.com>
2024-02-23 05:27:55 -0400

    Add Apps, description, image and links

--- main.py
+++ main.py
@@ -22,19 +22,25 @@ with col2:
     st.info(about_me)
 
 message1 = '''
-Below you can find some of the apps I have built in Python. 
+Below you can find some of the apps I have built(or plan to build) in Python. 
 Feel free to contact me!
 '''
 st.write(message1)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+        
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
