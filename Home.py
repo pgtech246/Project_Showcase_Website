4349632ad67d78e89748d65cd01f388942fec75e
paul <pgtech246@gmail.com>
2024-02-24 07:41:27 -0400

    Update about_me text

--- Home.py
+++ Home.py
@@ -15,8 +15,8 @@ with col2:
     Hi, I am Paul! I am a Python enthusiast. I have a degree in Biology and I work in the 
     field of education, in particular, in the area of science and technology. 
     I am currently learning Python programming after assisting in the deployment of a "Coding and
-    Robotics" programme at my workplace, Erdiston Teachers' Training College. That experience
-    with VEX Robotics and block-based coding using Scratch has led me on this journey to hone
+    Robotics" programme at my workplace, Erdiston Teachers' Training College. Working
+    with VEX Robots and block-based coding using Scratch has led me on this journey to hone
     newfound skills in the area of programming. I hope to eventually create or be
     part of a project(s) that can help to improve the lives of others!
     '''
